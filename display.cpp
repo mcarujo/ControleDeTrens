8d2ee6fb9a12cef96004412f861d224ac053d51f
Denis Ricardo da Silva Medeiros <denis@dimap.ufrn.br>
2017-10-30 15:35:05 -0300

    Versão final funcionando na placa.

--- display.cpp
+++ display.cpp
@@ -27,10 +27,10 @@ Display::Display(BlackLib::gpioName d1, BlackLib::gpioName d2,
     
     
     /* High select the digit */
-    digit1->setValue(BlackLib::low);
-    digit2->setValue(BlackLib::low);
-    digit3->setValue(BlackLib::low);
-    digit4->setValue(BlackLib::low);
+    digit1->setValue(BlackLib::high);
+    digit2->setValue(BlackLib::high);
+    digit3->setValue(BlackLib::high);
+    digit4->setValue(BlackLib::high);
     
     /* High voltage is off and low voltage is on */
     segA->setValue(BlackLib::high);
@@ -41,11 +41,7 @@ Display::Display(BlackLib::gpioName d1, BlackLib::gpioName d2,
     segF->setValue(BlackLib::high);
     segG->setValue(BlackLib::high);
     segDP->setValue(BlackLib::high);
-    
-    /* Initialize thread */
-    std::thread t(&Display::worker, this);
-    t.detach();
-    
+        
 }
 
 Display::~Display()
@@ -82,164 +78,58 @@ Display::~Display()
     delete segDP;
 }
 
-void Display::turnOnSegments(uint8_t number)
-{ 
-
-    switch(number)
+void Display::turnOn(uint8_t seg)
+{
+    switch(seg)
     {
-        case 0:
-
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segE->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        
-        break;
-         
         case 1:
-        
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        break;
-         
+            segA->setValue(BlackLib::low);
+            break;
         case 2:
-        
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segE->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-         
-        break;
-         
+            segB->setValue(BlackLib::low);
+            break;
         case 3:
-         
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-         
-        break;
-         
+            segC->setValue(BlackLib::low);
+            break;
         case 4:
-        
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-        break;
-         
+            segD->setValue(BlackLib::low);
+            break;
         case 5:
-        
-        segA->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-        break;
-         
+            segE->setValue(BlackLib::low);
+            break;
         case 6:
-        
-        segA->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segE->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-        break;
-         
+            segF->setValue(BlackLib::low);
+            break;
         case 7:
-        
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        break;
-         
-        case 8:
-        
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segE->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-        break;
-         
-        case 9:
-        
-        segA->setValue(BlackLib::low);
-        segB->setValue(BlackLib::low);
-        segC->setValue(BlackLib::low);
-        segD->setValue(BlackLib::low);
-        segF->setValue(BlackLib::low);
-        segG->setValue(BlackLib::low);
-        break;
-         
-        default:
-        break;
+            segG->setValue(BlackLib::low);
+            break;
     }
-    
-    usleep(1000);
-    segA->setValue(BlackLib::high);
-    segB->setValue(BlackLib::high);
-    segC->setValue(BlackLib::high);
-    segD->setValue(BlackLib::high);
-    segE->setValue(BlackLib::high);
-    segF->setValue(BlackLib::high);
-    segG->setValue(BlackLib::high);
-    segDP->setValue(BlackLib::high);
-    
 }
 
-void Display::worker()
+void Display::turnOff(uint8_t seg)
 {
-    uint8_t d1, d2, d3, d4; 
-    uint16_t localNumber;
-    
-    while(1)
+    switch(seg)
     {
-        mtx.lock();
-        localNumber = number;
-        mtx.unlock();
-
-        if(localNumber > 9999)
-        {
-            throw invalid_argument( "Number must be between 0 and 9999");
-        }
-        
-        d1 = (localNumber/1000) % 10;
-        d2 = (localNumber/100) % 10;
-        d3 = (localNumber/10) % 10;
-        d4 = (localNumber/1) % 10;
-    
-        digit1->setValue(BlackLib::high);
-        segDP->setValue(BlackLib::low); // Point here
-        turnOnSegments(d1);
-        digit1->setValue(BlackLib::low);
-        
-        digit2->setValue(BlackLib::high);
-        turnOnSegments(d2);
-        digit2->setValue(BlackLib::low);
-        
-        digit3->setValue(BlackLib::high);
-        turnOnSegments(d3);
-        digit3->setValue(BlackLib::low);
-        
-        digit4->setValue(BlackLib::high);
-        turnOnSegments(d4);
-        digit4->setValue(BlackLib::low);
-        
+        case 1:
+            segA->setValue(BlackLib::high);
+            break;
+        case 2:
+            segB->setValue(BlackLib::high);
+            break;
+        case 3:
+            segC->setValue(BlackLib::high);
+            break;
+        case 4:
+            segD->setValue(BlackLib::high);
+            break;
+        case 5:
+            segE->setValue(BlackLib::high);
+            break;
+        case 6:
+            segF->setValue(BlackLib::high);
+            break;
+        case 7:
+            segG->setValue(BlackLib::high);
+            break;
     }
-}
-
-
-void Display::showNumber(uint16_t _number)
-{
-    mtx.lock();
-    number = _number;   
-    mtx.unlock(); 
 }
--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
-//#include "BlackTime/BlackTime.h"
-//#include "BlackGPIO/BlackGPIO.h"
+#include "BlackTime/BlackTime.h"
+#include "BlackGPIO/BlackGPIO.h"
 #include "ADC/Adc.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
-//#include "display.h"
+#include "display.h"
 #include <mutex> 
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h> // biblioteca pthread
+#include <semaphore.h>
 
 #define UNIT_MS 1000
 #define UNIT_SEC 1000000
@@ -20,16 +21,18 @@
 using namespace std;
 
 /****************** Variaveis Globais ***********************/
-std::mutex mtx;
+pthread_mutex_t work_mutex;
 // Display
-/*Display display(BlackLib::GPIO_65, BlackLib::GPIO_45,
+Display display(BlackLib::GPIO_65, BlackLib::GPIO_45,
 					BlackLib::GPIO_69, BlackLib::GPIO_60, BlackLib::GPIO_27, BlackLib::GPIO_66,
 					BlackLib::GPIO_49, BlackLib::GPIO_115,
 					BlackLib::GPIO_20, BlackLib::GPIO_47,
 					BlackLib::GPIO_48, BlackLib::GPIO_46);
-		*/
-void *thread_tren2(void *arg);
-void *thread_tren1(void *arg);
+		
+		
+
+void *thread_trem1(void *arg);
+void *thread_trem2(void *arg);
 
 /****************** FIM de Globais ***********************/
 
@@ -39,10 +42,8 @@ int main()
 	pthread_t consumdior, produtor;
 	void * statusFinalizacao;
 	
-	//display.showNumber(0000);
-
 	printf("Programa principal criando thread consumidor...\n");
-	res = pthread_create(&consumdior, NULL, thread_tren2, (void *)&valor);
+	res = pthread_create(&consumdior, NULL, thread_trem2, (void *)&valor);
 	if (res != 0)
 	{
 		perror("A Craição da Thread consumidor falhou");
@@ -51,7 +52,7 @@ int main()
 
 
 	printf("Programa principal criando thread produtor...\n");
-	res = pthread_create(&produtor, NULL, thread_tren1, (void *)&valor);
+	res = pthread_create(&produtor, NULL, thread_trem1, (void *)&valor);
 	if (res != 0)
 	{
 		perror("A Craição da Thread produtor falhou");
@@ -65,7 +66,9 @@ int main()
         perror("O thread_join falhou");
         exit(EXIT_FAILURE);
     }
+    
 	res = pthread_join(produtor, &statusFinalizacao);
+	
     if (res != 0) {
         perror("O thread_join falhou");
         exit(EXIT_FAILURE);
@@ -74,26 +77,49 @@ int main()
     exit(EXIT_SUCCESS);
 }
 
-void *thread_tren1(void *valor)
+void *thread_trem1(void *valor)
 {
 	//BlackLib::BlackGPIO trenzinho1(BlackLib::GPIO_68, BlackLib::input, BlackLib::SecureMode);
+	
+	float perc1;
+	ADC pot1(AIN0); 
+
 	while (1)
 	{
-		//while(trenzinho1.getValue() == "1");
-
+		perc1 = pot1.getPercentValue();
+		 
+		if(perc1 < 1) perc1 = 1;
+		 
+		display.turnOn(1);
+		usleep(perc1  * 10 *  UNIT_MS);
+		display.turnOff(1); 
+		
+		display.turnOn(2);
+		usleep(perc1  * 10 *  UNIT_MS);
+		
+		pthread_mutex_lock(&work_mutex); // Entrando na região crítica
+		display.turnOff(2); 
+		display.turnOn(7);
+		usleep(perc1  * 10 *  UNIT_MS);
+		display.turnOff(7); 
+		pthread_mutex_unlock(&work_mutex); // saindo de região critica 
+		
+		display.turnOn(6);
+		usleep(perc1  * 10 *  UNIT_MS);
+		display.turnOff(6); 
+		
 		// SET L1 high
-		//sleep(trenzinho1);
 		//SET L1 low;
 		//sleep(trenzinho1);
 		//SET L4 high
 		//sleep(trenzinho1);
 		//SET L4 low
 		//sleep(trenzinho1);
-		mtx.lock();
+		//
 		//SET L3 high
 		//sleep(trenzinho1);
 		//SET L3 low
-		mtx.unlock();
+		//pthread_mutex_unlock(&work_mutex); // saindo de região critica
 		//sleep(trenzinho1);
 		//SET L4 high
 		//sleep(trenzinho1);
@@ -102,12 +128,38 @@ void *thread_tren1(void *valor)
 
 }
 
-void *thread_tren2(void *valor)
+void *thread_trem2(void *valor)
 {
 	
-	//BlackLib::BlackGPIO trenzinho2(BlackLib::GPIO_68, BlackLib::input, BlackLib::SecureMode);
+	float perc2;
+	ADC pot2(AIN1);
+	
 	while (1)
 	{
+		
+		perc2 = pot2.getPercentValue();
+		
+		if(perc2 < 1) perc2 = 1;
+		 
+		display.turnOn(4);
+		usleep(perc2  * 10 *  UNIT_MS);
+		display.turnOff(4); 
+		
+		display.turnOn(5);
+		usleep(perc2  * 10 *  UNIT_MS); 
+		
+		pthread_mutex_lock(&work_mutex); // Entrando na região crítica
+		display.turnOff(5);
+		display.turnOn(7);
+		usleep(perc2  * 10 *  UNIT_MS);
+		display.turnOff(7); 
+		pthread_mutex_unlock(&work_mutex); // saindo de região critica 
+		
+		display.turnOn(3);
+		usleep(perc2  * 10 *  UNIT_MS);
+		display.turnOff(3); 
+		
+	
 		//while(trenzinho2.getValue() == "1");
 		// SET L6 high
 		//sleep(trenzinho2);
@@ -117,11 +169,11 @@ void *thread_tren2(void *valor)
 		//sleep(trenzinho2);
 		//SET L5 low
 		//sleep(trenzinho2);
-		mtx.lock();
+		pthread_mutex_lock(&work_mutex); // Entrando na região crítica
 		//SET L3 high
 		//sleep(trenzinho2);
 		//SET L3 low
-		mtx.unlock();
+		pthread_mutex_unlock(&work_mutex); // saindo de região critica
 		//sleep(trenzinho2);
 		//SET L3 high
 		//sleep(trenzinho2);
