01f29119417a443425d052a13c4ee631f20be005
Denis Ricardo da Silva Medeiros <denis@dimap.ufrn.br>
2017-11-01 08:42:48 -0300

    Versão final.

--- main.cpp
+++ main.cpp
@@ -23,6 +23,11 @@ using namespace std;
 /****************** Variaveis Globais ***********************/
 pthread_mutex_t work_mutex;
 
+pthread_t trem1, trem2,reset;
+BlackLib::BlackGPIO entrada(BlackLib::GPIO_68, BlackLib::input, BlackLib::SecureMode);
+
+
+
 // Display
 Display display(BlackLib::GPIO_65, BlackLib::GPIO_45,
 					BlackLib::GPIO_69, BlackLib::GPIO_60, BlackLib::GPIO_27, BlackLib::GPIO_66,
@@ -39,47 +44,67 @@ void *thread_trem2(void *arg);
 
 int main()
 {
+
 	int res, valor=0;
 	pthread_t trem1, trem2;
 	void * statusFinalizacao;
 	
-	printf("Programa principal criando thread consumidor...\n");
-	res = pthread_create(&trem1, NULL, thread_trem2, (void *)&valor);
-	if (res != 0)
-	{
-		perror("A Craição da Thread consumidor falhou");
-		exit(EXIT_FAILURE);
-	}
+      while(1) {
 
 
-	printf("Programa principal criando thread trem2...\n");
-	res = pthread_create(&trem2, NULL, thread_trem1, (void *)&valor);
-	if (res != 0)
-	{
-		perror("A Craição da Thread trem2 falhou");
-		exit(EXIT_FAILURE);
-	}
+			printf("Programa principal criando thread trem1...\n");
+			res = pthread_create(&trem1, NULL, thread_trem1, (void *)&valor);
+			if (res != 0)
+			{
+				perror("A Craição da Thread trem1 falhou");
+				exit(EXIT_FAILURE);
+			}
 
 
-	printf("Programa principal esperando pelo término das threads...\n");
-    res = pthread_join(trem1, &statusFinalizacao);
-    if (res != 0) {
-        perror("O thread_join falhou");
-        exit(EXIT_FAILURE);
-    }
-    
-	res = pthread_join(trem2, &statusFinalizacao);
+			printf("Programa principal criando thread trem2...\n");
+			res = pthread_create(&trem2, NULL, thread_trem2, (void *)&valor);
+			if (res != 0)
+			{
+				perror("A Craição da Thread trem2 falhou");
+				exit(EXIT_FAILURE);
+			}
+			while(entrada.getValue() == "1");
+			/*
+			printf("Programa principal esperando pelo término das threads...\n");
+			res = pthread_join(trem1, &statusFinalizacao);
+			if (res != 0) {
+				perror("O thread_join falhou");
+				exit(EXIT_FAILURE);
+			}
+			    
+			res = pthread_join(trem2, &statusFinalizacao);
 	
-    if (res != 0) {
-        perror("O thread_join falhou");
-        exit(EXIT_FAILURE);
-    }
-   
-    exit(EXIT_SUCCESS);
+			if (res != 0) {
+				perror("O thread_join falhou");
+				exit(EXIT_FAILURE);
+			}
+			*/
+	   		pthread_cancel(trem1);
+			pthread_cancel(trem2);
+			pthread_mutex_unlock(&work_mutex);
+			display.turnOff(1); 
+			display.turnOff(2); 
+			display.turnOff(3); 
+			display.turnOff(4); 
+			display.turnOff(5); 
+			display.turnOff(6); 
+			display.turnOff(7); 
+			sleep(1);
+	      }
+
 }
 
 void *thread_trem1(void *valor)
 {
+
+	 int prevType;
+   	 pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &prevType);
+
 	float perc1;
 	ADC pot1(AIN0); 
 
@@ -113,7 +138,10 @@ void *thread_trem1(void *valor)
 
 void *thread_trem2(void *valor)
 {
-	
+
+	 int prevType;
+   	 pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &prevType);
+
 	float perc2;
 	ADC pot2(AIN1);
 	
@@ -141,7 +169,12 @@ void *thread_trem2(void *valor)
 		display.turnOn(3);
 		usleep(perc2  * 10 *  UNIT_MS);
 		display.turnOff(3); 
-		
+
+
+
+
 	}
 
 }
+
+
